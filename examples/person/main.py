09489b95d5deebf57846733e842c9a34c9b62f6e
blamedcloud <denverwoodward@yahoo.com>
2019-02-14 18:46:37 -0500

    added clarifying comment to person example

--- examples/person/main.py
+++ examples/person/main.py
@@ -7,6 +7,7 @@ def main():
 	joe = pp.getPerson('testPerson.txt')
 	print('Before Change:')
 	print(joe)
+	# Joe's file hasn't been updated since his birthday, so we update the age field:
 	joe.age = 19
 	print('After Change:')
 	print(joe)
